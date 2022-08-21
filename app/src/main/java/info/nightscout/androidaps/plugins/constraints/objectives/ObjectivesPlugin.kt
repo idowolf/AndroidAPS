bca24a8c5fd26d1ab2c17ec1718ded531cbb7a3c
Ido Wolf <idowolf@gmail.com>
2022-08-21 15:43:58 +0300

    Switch order

--- app/src/main/java/info/nightscout/androidaps/plugins/constraints/objectives/ObjectivesPlugin.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/constraints/objectives/ObjectivesPlugin.kt
@@ -70,8 +70,8 @@ class ObjectivesPlugin @Inject constructor(
     private fun setupObjectives() {
         objectives.clear()
         objectives.add(Objective0(injector))
-        objectives.add(Objective1(injector))
         objectives.add(Objective2(injector))
+        objectives.add(Objective1(injector))
         objectives.add(Objective3(injector))
         objectives.add(Objective4(injector))
         objectives.add(Objective5(injector))
