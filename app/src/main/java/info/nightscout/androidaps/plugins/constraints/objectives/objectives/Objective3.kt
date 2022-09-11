ba5d90601f9a79f08af38f110f5a48fec39df392
Ido Wolf <idowolf@gmail.com>
2022-09-11 13:01:30 +0300

    Bool

--- app/src/main/java/info/nightscout/androidaps/plugins/constraints/objectives/objectives/Objective3.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/constraints/objectives/objectives/Objective3.kt
@@ -27,7 +27,7 @@ class Objective3 @Inject constructor(injector: HasAndroidInjector) : Objective(i
     }
 
     override fun specialActionEnabled(): Boolean =
-        nsClientPlugin.nsClientService?.isConnected == true && nsClientPlugin.nsClientService?.hasWriteAuth == true
+        true
 
     override fun specialAction(activity: FragmentActivity, input: String) {
         objectivesPlugin.completeObjectives(activity, input)
