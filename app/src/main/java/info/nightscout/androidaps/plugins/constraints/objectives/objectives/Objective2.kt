a0b5b526f0214a72d63112e866b88244c03f4756
Ido Wolf <idowolf@gmail.com>
2022-09-11 13:09:39 +0300

    Bool

--- app/src/main/java/info/nightscout/androidaps/plugins/constraints/objectives/objectives/Objective2.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/constraints/objectives/objectives/Objective2.kt
@@ -7,12 +7,5 @@ import info.nightscout.androidaps.R
 class Objective2(injector: HasAndroidInjector) : Objective(injector, "exam", R.string.objectives_exam_objective, R.string.objectives_exam_gate) {
 
     init {
-        tasks.add(ExamTask(this, R.string.prerequisites_label, R.string.prerequisites_what, "prerequisites")
-            .option(Option(R.string.prerequisites_nightscout, true))
-            .option(Option(R.string.prerequisites_computer, true))
-            .option(Option(R.string.prerequisites_pump, true))
-            .option(Option(R.string.prerequisites_beanandroiddeveloper, false))
-            .hint(Hint(R.string.prerequisites_hint1))
-        )
     }
 }
